a2def681fc70fa073cf4aaecd221d96280e32922
lswatson16 <lauralynw16@gmail.com>
2022-04-26 12:35:50 -0400

    Revise input name and value for listener and handler event

--- src/components/UrlForm/UrlForm.js
+++ src/components/UrlForm/UrlForm.js
@@ -36,8 +36,8 @@ class UrlForm extends Component {
         <input
           type='text'
           placeholder='URL to Shorten...'
-          name='title'
-          value={this.state.title}
+          name='urlToShorten'
+          value={this.state.urlToShorten}
           onChange={e => this.handleNameChange(e)}
         />
 
